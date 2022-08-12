85e531c8e8a62f67880c9a7570d9cabaa31ffca8
nayana005 <nayanasuresh1005@gmail.com>
2022-08-13 00:50:18 +0530

    Added: Equality of two lines

--- LineComparison.java
+++ LineComparison.java
@@ -2,34 +2,35 @@ package com.bl.line_comparison;
 
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
 public class LineComparison {
-    public static void main (String[] args)
-    {
-        System.out.println("Welcome to LineComparison Computation Program");
-        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
-        System.out.print("Enter first Xaxis coordinator number as x1:  ");
-        int x1= sc.nextInt();
-        System.out.print("Enter second  Xaxis coordinator number as x2:  ");
-        int x2= sc.nextInt();
-        System.out.print("Enter first Yaxis coordinator number as y1: ");
-        int y1= sc.nextInt();
-        System.out.print("Enter Second Yaxis coordinator number as y2: ");
-        int y2= sc.nextInt();
-        double length1=Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-        System.out.println("Total= " +length1);
-        System.out.println("Total Length of line 1 is : " +length1);
-        System.out.print("Enter  cordinates of x3:  ");
-        int x3= sc.nextInt();
-        System.out.print("Enter cordinates of x4:  ");
-        int x4= sc.nextInt();
-        System.out.print("Enter cordinates of  y3: ");
-        int y3= sc.nextInt();
-        System.out.print("Enter cordinates of  y4: ");
-        int y4= sc.nextInt();
-        double length2=Math.sqrt(((x4-x3)*(x4-x3))+((y4-y3)*(y4-y3)));
-        System.out.println("Total Length of line 2 is : " +length2);
-       
+    public static void main (String[] args) {
+     System.out.println("Welcome to LineComparison Computation Program");
+     Scanner sc = new Scanner(System.in);    
+     System.out.print("Enter coordinates of x1:  "); 
+     int x1 = sc.nextInt();
+     System.out.print("Enter coordinates of x2:  ");
+     int x2 = sc.nextInt();
+     System.out.print("Enter coordinates of y1: ");
+     int y1 = sc.nextInt();
+     System.out.print("Enter coordinates of y2: ");
+     int y2 = sc.nextInt();
+     double length1 = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+     System.out.println("Total Length of line 1 is : " +length1);
+     System.out.print("Enter  cordinates of x3:  ");
+     int x3 = sc.nextInt();
+     System.out.print("Enter cordinates of x4:  ");
+     int x4 = sc.nextInt();
+     System.out.print("Enter cordinates of  y3: ");
+     int y3 = sc.nextInt();
+     System.out.print("Enter cordinates of  y4: ");
+     int y4 = sc.nextInt();
+     double length2 = Math.sqrt(((x4-x3)*(x4-x3))+((y4-y3)*(y4-y3)));
+     System.out.println("Total Length of line 2 is : " +length2);
+     if( length1 == length2 ){
+        System.out.println("TWO Lines are Equal");
+    }
+    else{
+        System.out.println("TWO Lines are Not Equal");
+    }
 }
 }
